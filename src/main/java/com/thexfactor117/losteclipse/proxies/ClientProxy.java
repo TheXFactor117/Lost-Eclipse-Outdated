86817fbeda10e96f61799a6349fe6ecfc21893df
TheXFactor117 <thexfactor117.mods@gmail.com>
2016-06-21 17:20:14 -0400

    Testing drone.io

--- src/main/java/com/thexfactor117/losteclipse/proxies/ClientProxy.java
+++ src/main/java/com/thexfactor117/losteclipse/proxies/ClientProxy.java
@@ -1,14 +1,19 @@
 package com.thexfactor117.losteclipse.proxies;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 /**
  * 
  * @author TheXFactor117
  *
  */
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy
 {
 	@Override
 	public void registerRenderers()
 	{
+		//RenderingRegistry.registerEntityRenderingHandler(EntityMagic.class, RenderModProjectile::new);
 	}
 }
