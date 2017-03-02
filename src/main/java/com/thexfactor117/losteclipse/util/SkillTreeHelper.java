f4019a32e202cf5e3be3ec78b598d7cfcd83d74d
TheXFactor117 <thexfactor117.mods@gmail.com>
2017-03-02 18:55:23 -0500

    Adding SkillTreeHelper

---
+++ src/main/java/com/thexfactor117/losteclipse/util/SkillTreeHelper.java
@@ -0,0 +1,31 @@
+package com.thexfactor117.losteclipse.util;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+public class SkillTreeHelper 
+{
+	// warrior
+	public static final float FORTIFICATION_1 = 0.1F;
+	public static final float FORTIFICATION_2 = 0.2F; // Increases damage resistance (or decreases how much damage is actually dealt).
+	public static final float FORTIFICATION_3 = 0.3F;
+	public static final float HEAVY_LIFTING_1 = 0.1F; // Increases movement speed when wearing a full set of heavy armor.
+	public static final float HEAVY_LIFTING_2 = 0.2F;
+	public static final float VIGOR_1 = 2F;
+	public static final float VIGOR_2 = 2F;
+	public static final float VIGOR_3 = 3F; // Increases the amount of max health a player has.
+	public static final float VIGOR_4 = 3F;
+	public static final float VIGOR_5 = 4F;
+	public static final float REGENERATION_1 = 0.1F;
+	public static final float REGENERATION_2 = 0.2F; // Increases the amount of health restored per sec, regardless of whether or not food has been eaten.
+	public static final float REGENERATION_3 = 0.3F;
+	public static final float POWER_1 = 0.1F;
+	public static final float POWER_2 = 0.2F; // Increases the amount of damage is dealt.
+	public static final float POWER_3 = 0.3F;
+	public static final float SWORDSMAN_1 = 0.1F; // Increases the amount of damage swords deal.
+	public static final float SWORDSMAN_2 = 0.2F;
+	public static final float BRUTE_1 = 0.1F; // Increases the amount of damage maces deal.
+	public static final float BRUTE_2 = 0.2F;
+}
